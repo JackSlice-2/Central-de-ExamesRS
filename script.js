58cfdd978358b6de79e43780acdeecc7dc3cebc9
Jack Slice <jackslice9@gmail.com>
2023-09-04 20:50:08 -0300

    Production Ready

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-let searchForm = document.querySelector(".search-form");
-document.querySelector("#search-btn").onclick = () =>{
-    searchForm.classList.toggle("active");
-    navbar.classList.remove("active");
-}
-
 let navbar = document.querySelector(".navbar");
 document.querySelector("#menu-btn").onclick = () =>{
     navbar.classList.toggle("active");
